f9c7b52faa8683c75711e44cb91b1a9f0cc9ddc6
Lijiaxi127 <1092911409@qq.com>
2023-06-17 20:32:39 +0800

    增加病例列表

--- mock/table.js
+++ mock/table.js
@@ -3,11 +3,16 @@ const Mock = require('mockjs')
 const data = Mock.mock({
   'items|30': [{
     id: '@id',
-    title: '@sentence(10, 20)',
-    'status|1': ['published', 'draft', 'deleted'],
-    author: 'name',
-    display_time: '@datetime',
-    pageviews: '@integer(300, 5000)'
+    name: '@cname',
+    age: '@integer(1,100)',
+    'sex|1': ['男', '女'],
+    f_num: '@integer(300, 5000)',
+    'invasive|1': ['是', '否'],
+    'level|1': ['良性', '非良性'],
+    'consist|1': ['是', '否'],
+    device: '@string(2)',
+    modality: '@integer(1,4)',
+    slice: '@integer(100,300)'
   }]
 })
 
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -7,38 +7,196 @@
       border
       fit
       highlight-current-row
+      @selection-change="handleSelectionChange"
     >
+
+      <el-table-column
+        type="selection"
+        align="center"
+        :selectable="checkSelectable"
+        width="55"
+      />
+
       <el-table-column align="center" label="ID" width="95">
+        <el-table-column align="center" label="患者姓名" width="95">
+          <el-table-column label="F号" width="100" align="center">
+            <template slot-scope="scope">
+              {{ scope.$index }} <br> {{ scope.row.name }} <br> {{ scope.row.f_num }}
+            </template>
+          </el-table-column>
+        </el-table-column>
+      </el-table-column>
+
+      <el-table-column label="患者临床信息" width="400" align="center">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          <div>年龄：{{ scope.row.age }} 性别：{{ scope.row.sex }}</div> <br>
+          <div>出生年月：{{ scope.row.age }} 检查日期：{{ scope.row.sex }}</div><br>
+          <div>主诉：<el-input v-model="scope.row.vig" size="mini" placeholder="请输入" class="square" style="display:inline" /></div><br>
+          <div>备注：<el-input v-model="scope.row.vig" size="mini" placeholder="请输入" class="square" style="display:inline" /></div><br>
+          <div><el-button type="primary" size="small">上传</el-button></div>
+
+          <!-- <span>{{ scope.row.author }}</span> -->
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+
+      <el-table-column label="成像设备" width="110" align="center">
+        <el-table-column align="center" label="模态个数" width="110">
+          <el-table-column align="center" label="切片个数" width="110">
+            <template slot-scope="scope">
+              {{ scope.row.device }} <br> {{ scope.row.modality }} <br> {{ scope.row.slice }}
+
+              <div><el-button type="primary" size="small">查看图片</el-button></div>
+
+            </template>
+          </el-table-column>
+        </el-table-column>
+      </el-table-column>
+
+      <el-table-column class-name="status-col" label="AI诊断结果" width="150" align="center">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          <div>脑侵袭：<el-tag :type="[scope.row.invasive==='否' ? 'success': 'danger']">{{ scope.row.invasive }}</el-tag></div> <br>
+          <div>分级：<el-tag :type="[scope.row.level==='良性' ? 'success': 'danger']">{{ scope.row.level }}</el-tag></div>
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+
+      <el-table-column label="手术信息" width="400" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <div class="block">手术日期：
+            <el-date-picker
+              v-model="scope.row.data"
+              type="date"
+              placeholder="选择日期"
+              size="mini"
+            />
+          </div> <br>
+
+          <div>病变部位：
+            <el-select
+              v-model="scope.row.region"
+              filterable
+              allow-create
+              default-first-option
+              size="mini"
+              placeholder="请选择"
+              class="square"
+            >
+              <el-option
+                v-for="item in p_region"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select></div><br>
+
+          <div>蛛网膜隔离：
+            <el-select
+              v-model="scope.row.zhuw"
+              filterable
+              allow-create
+              default-first-option
+              size="mini"
+              placeholder="请选择"
+              class="square"
+            >
+              <el-option
+                v-for="item in p_zhuw"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select></div>
+
+          <div>辛普森分级：
+            <el-select
+              v-model="scope.row.xinp"
+              filterable
+              allow-create
+              default-first-option
+              size="mini"
+              placeholder="请选择"
+              class="square"
+            >
+              <el-option
+                v-for="item in p_xinp"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select></div>
+
+          <div><el-button type="primary" size="small">上传</el-button></div>
+
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+
+      <el-table-column label="术后病理结果" width="200" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          <div>脑侵袭：<el-select
+            v-model="scope.row.p_invasive"
+            filterable
+            allow-create
+            default-first-option
+            size="mini"
+            placeholder="请选择"
+            class="square"
+          >
+            <el-option
+              v-for="item in p_inv_op"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select></div> <br>
+
+          <div>分级：
+            <el-select
+              v-model="scope.row.p_level"
+              filterable
+              allow-create
+              default-first-option
+              size="mini"
+              placeholder="请选择"
+              class="square"
+            >
+              <el-option
+                v-for="item in p_lev_op"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select></div><br>
+
+          <div>增值指数：<el-input v-model="scope.row.vig" size="mini" placeholder="请输入" class="square" style="display:inline" /></div><br>
+
+          <div>病理亚型：
+            <el-select
+              v-model="scope.row.p_sub"
+              filterable
+              allow-create
+              default-first-option
+              size="mini"
+              placeholder="请选择"
+              class="square"
+            >
+              <el-option
+                v-for="item in p_sub_op"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select></div>
+
+          <div><el-button type="primary" size="small">上传</el-button></div>
+
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+
+      <el-table-column align="center" prop="created_at" label="是否一致" width="200">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+          <el-tag :type="[scope.row.consist==='否' ? 'success': 'danger']">{{ scope.row.consist }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
+
     </el-table>
   </div>
 </template>
@@ -60,7 +218,40 @@ export default {
   data() {
     return {
       list: null,
-      listLoading: true
+      listLoading: true,
+      p_inv_op: [
+        { value: '选项1', label: '是' },
+        { value: '选项2', label: '否' }],
+      p_lev_op: [
+        { value: '选项1', label: '一级' },
+        { value: '选项2', label: '二级' },
+        { value: '选项3', label: '三级' }],
+      p_sub_op: [
+        { value: '选项1', label: '脑膜皮型' },
+        { value: '选项2', label: '纤维型 (成纤维型)' },
+        { value: '选项3', label: '过渡型 (混合型)' },
+        { value: '选项4', label: '沙粒体型' },
+        { value: '选项5', label: '血管瘤型' },
+        { value: '选项6', label: '微囊型' },
+        { value: '选项7', label: '分泌型' },
+        { value: '选项8', label: '富淋巴-浆细胞型' },
+        { value: '选项9', label: '移行型' },
+        { value: '选项10', label: '透明细胞型' },
+        { value: '选项11', label: '脊索瘤样型' },
+        { value: '选项12', label: '非典型' },
+        { value: '选项13', label: '乳头状瘤型' },
+        { value: '选项14', label: '横纹肌样型' },
+        { value: '选项15', label: '间变性 (恶性)' }],
+      p_zhuw: [
+        { value: '选项1', label: '是' },
+        { value: '选项2', label: '否' }],
+      p_xinp: [
+        { value: '选项1', label: '一级' },
+        { value: '选项2', label: '二级' },
+        { value: '选项3', label: '三级' },
+        { value: '选项3', label: '四级' }],
+      p_region: [
+        { value: '选项1', label: '无' }]
     }
   },
   created() {
@@ -77,3 +268,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .square /deep/ .el-input__inner {
+    width: 90px !important;
+  }
+
+  .el-button--primary{
+  margin-top: 20px;
+}
+
+</style>
