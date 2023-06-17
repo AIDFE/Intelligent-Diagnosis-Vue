5cd1feb2a5163f60e71eda291aa28a66b0a0657f
Lijiaxi127 <1092911409@qq.com>
2023-06-17 10:30:23 +0800

    更改layout样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,7 +3,7 @@
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <div class="bar-head">
       <div style="display:flex">
-        <img v-if="logo" :src="logo" style="width:385px;height:67px;margin-top:11px;margin-left:7px">
+        <img v-if="logo" :src="logo" style="width:80px;height:80px;margin-top:0px;margin-left:20px">
       </div>
       <div class="right-menu">
         <el-dropdown class="avatar-container" trigger="click" placement="bottom-end" :hide-on-click="true">
