7b79adffa8e522d904b1c1a5c8ad84fd6692b503
Lijiaxi127 <1092911409@qq.com>
2023-06-18 21:44:18 +0800

    layout标题

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,7 +4,7 @@
     <div class="bar-head">
       <div style="display:flex">
         <img v-if="logo" :src="logo" style="width:80px;height:80px;margin-top:0px;margin-left:20px">
-      </div>
+        <h1>  脑膜瘤智能分级诊断系统 (IGDS-M)</h1></div>
       <div class="right-menu">
         <el-dropdown class="avatar-container" trigger="click" placement="bottom-end" :hide-on-click="true">
           <div class="avatar-wrapper">
@@ -100,6 +100,7 @@ export default {
       top: 0;
     }
   }
+
   .drawer-bg {
     background: #000;
     opacity: 0.3;
